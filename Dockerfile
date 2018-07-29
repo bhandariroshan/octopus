30da34a312ff9a252425c40b81b1c97f2d40c782
rbhandari <rbhandari@growbydata.com>
2018-07-29 13:27:57 +0545

    docker compose support add

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,10 @@
 FROM python:3.5
 
-RUN apt-get update
-RUN apt-get install mysql-server
-
 RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app
 
 COPY requirements.txt /usr/src/app/
-RUN pip install --no-cache-dir -r requirements.txt
+RUN pip3 install --no-cache-dir -r requirements.txt
 
 CMD ["python3", "/runserver.py"]
 
--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ from tornado.options import define, options, parse_command_line # tornado option
 
 COOKIE_SECRET = 'roshan'
 define("port", default=8888, help="run on the given port", type=int)
-define("db_connection_str", default="mysql://octopus:octopus123@localhost/octopuslabs", help="Database connection string for application") # db connection string
+define("db_connection_str", default="mysql://root:password@db/octopuslabs", help="Database connection string for application") # db connection string
 define("executor_max_threads", default=20, help="max threads for threadpool executor") # max number of threads for executor
 
 parse_command_line() # firstly, get the above options from command line
